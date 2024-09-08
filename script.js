32d6124c39649a7ad15cbfa75b83ea4929340bf8
Rhem <itsrhemiagain@gmail.com>
2024-09-08 22:05:54 +0800

    Finish all content for the restart and content

--- script.js
+++ script.js
@@ -5,4 +5,27 @@ document.addEventListener('DOMContentLoaded', function () {
     hamburger.addEventListener('click', function () {
         navigation.classList.toggle('active');
     });
+
+    const header = document.querySelector('.header');
+    let lastScrollY = window.scrollY;
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY < lastScrollY) {
+            header.style.top = '0';
+        } else {
+            header.style.top = '-100px';
+        }
+        lastScrollY = window.scrollY;
+    })
 });
+
+const observer = new IntersectionObserver (entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach(el => observer.observe(el));
